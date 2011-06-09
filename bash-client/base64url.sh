280762243fcc944178659df7d6ec11089a4c5fe2
Pierre Chapuis <catwell@archlinux.us>
2011-06-09 09:59:03 +0200

    Add a script that does base64url {en,de}coding

---
+++ bash-client/base64url.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Encode to / decode from Base64-URL without padding.
+
+# USAGE:
+# bash base64url.sh encode 'Hello!'
+# bash base64url.sh decode SGVsbG8h
+
+function encode {
+  echo -n "$1" | openssl enc -a | tr -d '=' | tr '+/' '-_'
+}
+
+function decode {
+  _l=$((${#1} % 4))
+  if [ $_l -eq 2 ]; then _s="$1"'=='
+  elif [ $_l -eq 3 ]; then _s="$1"'='
+  else _s="$1" ; fi
+  echo "$_s" | openssl enc -d -a
+}
+
+case $1 in
+  encode) encode $2 ;;
+  decode) decode $2 ;;
+  e) encode $2 ;;
+  d) decode $2 ;;
+esac
